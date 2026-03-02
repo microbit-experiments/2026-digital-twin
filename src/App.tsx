96826fc842f22b5c235dbead715d3acfeb13f28f
Grace <grace@microbit.org>
2026-03-02 11:40:01 +0000

    Add TODO for connect and flash micro:bit

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,15 @@ function App() {
   const handleButtonAMouseUp = useCallback(() => {
     microbitDrawing.reset();
   }, [microbitDrawing]);
+  const handleConnect = useCallback(async () => {
+    // TODO: Follow documentation in https://github.com/microbit-foundation/microbit-connection
+    // to connect and flash the meet the micro:bit hex using USB. 
+    // Version to install: https://www.npmjs.com/package/@microbit/microbit-connection.
+  }, []);
   return (
+    // TODO: User interface.
     <VStack width="100%" height="100%">
+      <Button onClick={handleConnect}>Connect</Button>
       <MicrobitSVG width="400px" />
       <Button
         onMouseDown={handleButtonAMouseDown}
