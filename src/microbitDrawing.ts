4a49ee4249b046341f10370f7234e5ca2a0d4261
Grace <grace@microbit.org>
2026-03-02 17:04:33 +0000

    Fix issue where buttonA has not been rendered yet.

--- src/microbitDrawing.ts
+++ src/microbitDrawing.ts
@@ -1,11 +1,21 @@
 // For manipulating microbit-drawing.svg.
 export class MicrobitDrawing {
-  private buttonA: HTMLElement;
+  private cachedButtonA: HTMLElement | null;
   constructor() {
-    this.buttonA = document.querySelector("#ButtonA")!;
+    this.cachedButtonA = document.querySelector("#ButtonA");
+  }
+  private buttonA() {
+    if (this.cachedButtonA) {
+      return this.cachedButtonA;
+    }
+    const buttonAEl = document.querySelector("#ButtonA");
+    if (!buttonAEl) {
+      throw new Error("Button A does not exist!");
+    }
+    return buttonAEl;
   }
   private fillButtonA(color: string) {
-    const buttonACircle = this.buttonA.querySelector("circle");
+    const buttonACircle = this.buttonA().querySelector("circle");
     if (buttonACircle) {
       buttonACircle.style.fill = color;
     }
