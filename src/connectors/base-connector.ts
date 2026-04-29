2e878cf55cee2b49e9308f5988eb18295896444e
InfinityFalls <anthony2006chiang@outlook.com>
2026-04-29 10:14:41 +0100

    remove logs for registering handlers

--- src/connectors/base-connector.ts
+++ src/connectors/base-connector.ts
@@ -6,7 +6,7 @@ export abstract class BaseConnector extends MicrobitConnector {
     protected onConnect?: () => void;
     protected onConnecting?: () => void;
     protected onPause?: () => void;
-    
+
     protected buttonADown?: () => void;
     protected buttonAUp?: () => void;
 
@@ -45,147 +45,118 @@ export abstract class BaseConnector extends MicrobitConnector {
 
     public setOnNoAuthorizedDevice(callback: () => void): void {
         this.onNoAuthorizedDevice = callback;
-        this.log("onNoAuthorizedDevice handler registered")
     }
 
     public setOnDisconnect(callback: () => void): void {
         this.onDisconnect = callback;
-        this.log("onDisconnect handler registered");
     }
 
     public setOnConnect(callback: () => void): void {
         this.onConnect = callback;
-        this.log("onConnect handler registered");
     }
 
     public setOnConnecting(callback: () => void): void {
         this.onConnecting = callback;
-        this.log("onConnecting handler registered");
     }
 
     public setOnPause(callback: () => void): void {
         this.onPause = callback;
-        this.log("onPause handler registered");
     }
 
 
     public setOnButtonADown(callback: () => void): void {
         this.buttonADown = callback;
-        this.log("buttonADown handler registered")
     }
 
     public setOnButtonAUp(callback: () => void): void {
         this.buttonAUp = callback;
-        this.log("buttonAUp handler registered")
     }
 
     public setOnButtonBDown(callback: () => void): void {
         this.buttonBDown = callback;
-        this.log("buttonBDown handler registered")
     }
 
     public setOnButtonBUp(callback: () => void): void {
         this.buttonBUp = callback;
-        this.log("buttonBUp handler registered")
     }
 
     public setLedMatrixUpdate(callback: (row: number, col: number, val: boolean) => void): void {
         this.ledMatrixUpdate = callback;
-        this.log("ledMatrixUpdate handler registered")
     }
 
     public setAccelerometerUpdate(callback: (x: number, y: number, z: number) => void): void {
         this.accelerometerUpdate = callback;
-        this.log("accelerometerUpdate handler registered")
     }
 
     public setMagnetometerUpdate(callback: (x: number, y: number, z: number) => void): void {
         this.magnetometerUpdate = callback;
-        this.log("magnetometerUpdate handler registered")
     }
 
     public setTemperatureUpdate(callback: (x: number) => void): void {
         this.temperatureUpdate = callback;
-        this.log("temperatureUpdate handler registered")
     }
 
     public setMicLedUpdate(callback: (val: boolean) => void): void {
         this.micLedUpdate = callback;
-        this.log("micLedUpdate handler registered");
     }
 
     public setTemplateLedUpdate(callback: (val: boolean) => void): void {
         this.templateLedUpdate = callback;
-        this.log("templateLedUpdate handler registered");
     }
 
     public setOnLogoDown(callback: () => void): void {
         this.onLogoDown = callback;
-        this.log("onLogoDown handler registered")
     }
 
     public setOnLogoUp(callback: () => void): void {
         this.onLogoUp = callback;
-        this.log("onLogoUp handler registered")
     }
 
     public setOnTiltUp(callback: () => void): void {
         this.onTiltUp = callback;
-        this.log("onTiltUp handler registered")
     }
 
     public setOnTiltDown(callback: () => void): void {
         this.onTiltDown = callback;
-        this.log("onTiltDown handler registered")
     }
 
     public setOnTiltLeft(callback: () => void): void {
         this.onTiltLeft = callback;
-        this.log("onTiltLeft handler registered")
     }
 
     public setOnTiltRight(callback: () => void): void {
         this.onTiltRight = callback;
-        this.log("onTiltRight handler registered")
     }
 
     public setOnFaceUp(callback: () => void): void {
         this.onFaceUp = callback;
-        this.log("onFaceUp handler registered")
     }
 
     public setOnFaceDown(callback: () => void): void {
         this.onFaceDown = callback;
-        this.log("onFaceDown handler registered")
     }
 
     public setOnFreefall(callback: () => void): void {
         this.onFreefall = callback;
-        this.log("onFreefall handler registered")
     }
 
     public setOnAcceleration3g(callback: () => void): void {
         this.onAcceleration3g = callback;
-        this.log("onAcceleration3g handler registered")
     }
 
     public setOnAcceleration6g(callback: () => void): void {
         this.onAcceleration6g = callback;
-        this.log("onAcceleration6g handler registered")
     }
 
     public setOnAcceleration8g(callback: () => void): void {
         this.onAcceleration8g = callback;
-        this.log("onAcceleration8g handler registered")
     }
 
     public setOnShake(callback: () => void): void {
         this.onShake = callback;
-        this.log("onShake handler registered")
     }
 
     public setOnAcceleration2g(callback: () => void): void {
         this.onAcceleration2g = callback;
-        this.log("onAcceleration2g handler registered")
     }
 }
